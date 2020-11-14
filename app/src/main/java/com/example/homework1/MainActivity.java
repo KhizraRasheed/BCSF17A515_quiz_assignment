fe24189f60b42e70c146f7e9f410c8f7cf09d8ba
KhizraRasheed <khizrasheed837@gmail.com>
2020-11-14 21:10:29 +0500

    my quiz assignment no 2. BCSF17A515

---
+++ app/src/main/java/com/example/homework1/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.homework1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView title;
+    private Button start;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        title= findViewById(R.id.textview);
+        start=findViewById(R.id.button);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent= new Intent(MainActivity.this,Ques.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
